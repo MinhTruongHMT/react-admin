c67f232a6be1ff486e7c79d62cbdfe387609b113
MinhTruongHMT <ninjarua2118@gmail.com>
2023-05-29 11:01:30 +0700

    create button

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
+
+//import 'antd/dist/antd.css';
 import './App.css';
+import { Button, Space } from 'antd';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Space className="site-button-ghost-wrapper" wrap>
+          <Button type="primary" ghost>
+            Primary
+          </Button>
+          <Button ghost>Default</Button>
+          <Button type="dashed" ghost>
+            Dashed
+          </Button>
+          <Button type="primary" danger ghost>
+            Danger
+          </Button>
+        </Space>
       </header>
     </div>
   );
